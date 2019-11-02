e60113c0c2e0316cd5ca9e7152de50fb961043e0
Roman-Kov <rojer_ko@bk.ru>
2019-11-02 19:42:46 +0300

    Собрал lesson-5 из исходников

--- lesson-5/src/main/java/Race.java
+++ lesson-5/src/main/java/Race.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Race {
+    private ArrayList<Stage> stages;
+    public ArrayList<Stage> getStages() { return stages; }
+    public Race(Stage... stages) {
+        this.stages = new ArrayList<Stage>(Arrays.asList(stages));
+    }
 }
