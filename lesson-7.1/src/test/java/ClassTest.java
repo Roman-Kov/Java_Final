fd370abdef49987e179089b569c15814115da74e
Roman-Kov <rojer_ko@bk.ru>
2019-11-10 14:39:43 +0300

    В процессе выполнения

---
+++ lesson-7.1/src/test/java/ClassTest.java
@@ -0,0 +1,5 @@
+public class ClassTest {
+    public static void start(Class testClass){
+        System.out.println("123");
+    }
+}
---
+++ lesson-7.1/src/test/java/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args){
+        ClassTest.start(Test1.class);
+    }
+}
